1ca81ca0d91af2df0cb4158a27ddd187eca8cd4e
GuilhermeO-Gomes <guilherme16062009@gmail.com>
2025-07-14 16:37:31 -0300

    Aplicação do Prettier

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
 // Seleciona o navbar
-const navbar = document.getElementById('navbar');
+const navbar = document.getElementById("navbar");
 
 // Adiciona um listener de scroll na janela
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
-    navbar.classList.add('scrolled'); // Adiciona a classe quando rolar
+    navbar.classList.add("scrolled"); // Adiciona a classe quando rolar
   } else {
-    navbar.classList.remove('scrolled'); // Remove a classe ao voltar
+    navbar.classList.remove("scrolled"); // Remove a classe ao voltar
   }
 });
 
 //////////////////////////////////////////////////////////////
-const track = document.querySelector('.carousel-track');
-  const cards = Array.from(track.children);
-  const prevButton = document.querySelector('.carousel-button.prev');
-  const nextButton = document.querySelector('.carousel-button.next');
+const track = document.querySelector(".carousel-track");
+const cards = Array.from(track.children);
+const prevButton = document.querySelector(".carousel-button.prev");
+const nextButton = document.querySelector(".carousel-button.next");
 
-  let currentIndex = 0;
+let currentIndex = 0;
 
-    const updateCarousel = () => {
-    const cardWidth = cards[0].getBoundingClientRect().width;
-    track.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-  };
+const updateCarousel = () => {
+  const cardWidth = cards[0].getBoundingClientRect().width;
+  track.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+};
 
-  nextButton.addEventListener('click', () => {
-    if (currentIndex < cards.length - 1) {
-      currentIndex++;
-      updateCarousel();
-    }
-  });
+nextButton.addEventListener("click", () => {
+  if (currentIndex < cards.length - 1) {
+    currentIndex++;
+    updateCarousel();
+  }
+});
 
-  prevButton.addEventListener('click', () => {
-    if (currentIndex > 0) {
-      currentIndex--;
-      updateCarousel();
-    }
-  });
+prevButton.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateCarousel();
+  }
+});
 
-  // Responsividade: atualiza o carrossel ao redimensionar a janela
-  window.addEventListener('resize', updateCarousel);
-
-
+// Responsividade: atualiza o carrossel ao redimensionar a janela
+window.addEventListener("resize", updateCarousel);
