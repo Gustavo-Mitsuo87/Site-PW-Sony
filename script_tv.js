96d23bc1a442f470c1db8ae49a4246545ecd0663
gabrieldiass02 <gabriel.diasrodrigues2010@gmail.com>
2025-08-28 18:29:46 -0300

    terminei real agr

--- script_tv.js
+++ script_tv.js
@@ -34,3 +34,26 @@ setInterval(() => {
 
 // Fim Carrossel Principais Recursos
 
+// Seleciona o navbar
+const navbar = document.getElementById("navbar");
+// Adiciona um listener de scroll na janela
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled"); // Adiciona a classe quando rolar
+  } else {
+    navbar.classList.remove("scrolled"); // Remove a classe ao voltar
+  }
+});
+
+
+const searchIcon = document.getElementById("search-icon");
+const searchInput = document.getElementById("search-input");
+
+searchIcon.addEventListener("click", () => {
+  searchInput.classList.toggle("active");
+  if (searchInput.classList.contains("active")) {
+    searchInput.focus();
+  }
+});
+
+
